2a606922517dba3c88243d2187fb950a7d6cf7bf
Martin Stöcker <martin.stoecker99@gmail.com>
2021-03-13 17:23:18 +0000

    commit implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 
 # [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+My implementation for fcc-challenge : https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
--- server.js
+++ server.js
@@ -8,25 +8,66 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
+
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date", function(req, res) {
+  let date_string = req.params.date
+  console.log("Request", date_string)
+
+
+  let date;
+  let unix;
+  let gmt;
+  // Different Parse Algos
+  //CHECK UNIX 
+  if (/^[0-9]*$/.test(date_string)) {
+    unix = Number(date_string)
+    gmt = new Date(Number(date_string)).toUTCString()
+  }
+  // CHECK DATE STRING
+  else if (Date.parse(date_string)) {
+    date = new Date(date_string)
+    unix = date.getTime()
+    gmt = date.toGMTString();
+  }
+  // ELSE OTHER
+  else {
+    res.json({ error: "Invalid Date" });
+  }
+
+  let output = {unix:unix,utc:gmt}
+  res.json(output);
+});
+
+// Hande timestamp Request if Date is empty
+app.get("/api/timestamp/", function(req, res) {
+  let date = new Date(Date.now())
+  let unix = date.getTime()
+  let gmt = date.toGMTString();
+  let output = { unix: unix, utc: gmt }
+  res.json(output);
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
